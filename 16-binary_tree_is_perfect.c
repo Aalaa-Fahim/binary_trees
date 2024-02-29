bd2dbbe40c28b2cf00fd499ead9fec894c2a229d
Reham <r_nour30@yahoo.com>
2024-02-29 14:53:42 -0800

    up

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,12 +17,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		{
 			return (1);
 		}
-		return (binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right));
-	}
-	else if (!(tree->parent))
-	{
-		return (1);
+		return ((binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right)));
 	}
 	return (0);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,18 +14,19 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
+	if (tree->right == NULL && tree->left == NULL)
+	{
+		return (0);
+	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+	{
+		return (left_height + 1);
+	}
 	else
 	{
-		left_height = binary_tree_height(tree->left);
-		right_height = binary_tree_height(tree->right);
-
-		if (left_height > right_height)
-		{
-			return (left_height + 1);
-		}
-		else
-		{
-			return (right_height + 1);
-		}
+		return (right_height + 1);
 	}
 }
