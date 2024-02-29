bc1d2b29ea48e79758dfb66280a15a3784291865
Reham <r_nour30@yahoo.com>
2024-02-29 09:08:49 -0800

    up

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -32,18 +32,18 @@ binary_tree_t *createNewNode(int value)
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newNode;
+
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
-	binary_tree_t *newNode = createNewNode(value);
-
-	newNode->parent = parent;
+	newNode = createNewNode(value);
 
 	if (parent->left != NULL)
 	{
-		freeNode(parent->left);
 		newNode->left = parent->left;
+		parent->left->parent = newNode;
 	}
 	parent->left = newNode;
 
