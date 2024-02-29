9c4c1972fc4ce71ef2048dcbc141dec93e252c4b
Reham <r_nour30@yahoo.com>
2024-02-29 09:30:52 -0800

    up

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -40,7 +40,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	newNode = malloc(sizeof(binary_tree_t));
 
-	if(newNode == NULL)
+	if (newNode == NULL)
 	{
 		return (NULL);
 	}
